aebba60f5222b94abeec66e2316f2c43eda49a68
kkats <kkats@DESKTOP-9HAEMB4>
2022-12-14 11:41:48 +0200

    Add project files.

---
+++ QuotesApplication_01_ICommmand/QuotesApplication_01_ICommmand/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace QuotesApplication_01_ICommmand
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,10 @@
+Xamarin MVVM ICommand interface
+
+
+With that small program i show how to use ICommand interface, instead of Click Event.
+
+
+
+
+Katsavaros Konstantinos
+
